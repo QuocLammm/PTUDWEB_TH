09f70e96a514dad75f755e246e5dfcd9ec018446
QuocLammm <quoclam010305@gmail.com>
2023-10-04 17:23:40 +0700

    Sửa và thêm

--- WEB_TH_63CNTT_4/Controllers/SiteController.cs
+++ WEB_TH_63CNTT_4/Controllers/SiteController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TextBD.Model;
+
 
 namespace WEB_TH_63CNTT_4.Controllers
 {
@@ -13,8 +15,16 @@ namespace WEB_TH_63CNTT_4.Controllers
         public ActionResult Index()
         {
             MyDBContext db = new MyDBContext(); // taoj mowis Database
+            
             int sodong = db.Products.Count();
             ViewBag.sodong = sodong;
+            return View();
+        }
+        public ActionResult Text()
+        {
+            Mycontext myContext = new Mycontext(); // Tạo mới database
+            int st = myContext.Menus.Count();  
+            ViewBag.st = st;
             return View();
         }
     }
